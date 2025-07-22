a07843b3f2761e248bfdb8359eb97d16591148f4
GodT <Godtrb@Gmail.com>
2025-07-21 21:45:38 -0600

    c

--- AAAAAAAAAAAAAA.py
+++ AAAAAAAAAAAAAA.py
@@ -17,11 +17,9 @@ def SumNAt(n):
             print(f"{n}+", end="")
         return n + SumNAt(n-1)
 def Fibo(n):
-    if n == 0:
+    if n<=1:
         return 1
-    elif n==1|n==2|n==3:
-        return "1"
-    elif (n>3):
+    else:
         return Fibo(n-1) + Fibo(n-2)
 def TextInvert(x):
     return x[::-1]
